af629c48f130dcdcf40031793cf555d0aaf7eb4d
baryshkov <ambaryshkov@gmail.com>
2019-11-14 20:59:08 +0300

    Added and styled inputs.

--- src/Components/LoginForm/LoginForm.jsx
+++ src/Components/LoginForm/LoginForm.jsx
@@ -1,21 +1,15 @@
 import React, { Component } from 'react';
 import InputMask from 'react-input-mask';
+import './LoginForm.css';
 import avatar from '../../resources/circle-cropped (3).png';
 
 class LoginForm extends Component {
   state = {
     userData: [
       {
-        name: 'Vasya',
-        phone: '+79666666666',
+        phone: '',
         rememberMe: false,
-        password: 'lol12345',
-      },
-      {
-        name: 'Prosche Pozvonit',
-        phone: '88005553535',
-        rememberMe: true,
-        password: 'lol12345',
+        password: '',
       },
     ],
   };
@@ -28,9 +22,19 @@ class LoginForm extends Component {
     return (
       <div className="container">
         <img src={avatar} className="" alt="logo" />
-        <form onSubmit={this.onSubmit}>
-          <InputMask mask="+7 ( 999 ) 999-99-99" placeholder="Номер телефона" />
-          <input type="password" />
+        <form onSubmit={this.onSubmit} className="login-form">
+          <InputMask
+            mask="+7 ( 999 ) 999-99-99"
+            className="login-form__input"
+            placeholder="Номер телефона"
+          />
+          <input type="password" className="login-form__input" placeholder="Пароль" />
+          <div className="login-form__remember">
+            <input type="checkbox" className="login-form__checkbox" id="rememberMe" />
+            <label htmlFor="rememberMe">Запомнить</label>
+            <a href="#">Забыл пароль</a>
+          </div>
+          <input type="submit" value="ВОЙТИ" className="login-form__submit" disabled />
         </form>
       </div>
     );
