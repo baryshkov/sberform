d711deac1dd9b6819c397921959671b69754c274
baryshkov <ambaryshkov@gmail.com>
2019-11-15 19:51:51 +0300

    Updates

---
+++ precache-manifest.07acd6b44e6d6795eed56d7ad9d3c1f5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ba9a24579b535ef5b4e96f1dd2e24cf",
+    "url": "/sberform/index.html"
+  },
+  {
+    "revision": "220c5a12e4d71f436c63",
+    "url": "/sberform/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "921d849ad40f9d38fa42",
+    "url": "/sberform/static/css/main.e90dccda.chunk.css"
+  },
+  {
+    "revision": "220c5a12e4d71f436c63",
+    "url": "/sberform/static/js/2.e3d8a5f9.chunk.js"
+  },
+  {
+    "revision": "921d849ad40f9d38fa42",
+    "url": "/sberform/static/js/main.798dbf00.chunk.js"
+  },
+  {
+    "revision": "47948d983dc12a22f23f",
+    "url": "/sberform/static/js/runtime-main.616f8c52.js"
+  },
+  {
+    "revision": "cbf00a1992318a967310988b1d78af25",
+    "url": "/sberform/static/media/Group.cbf00a19.svg"
+  },
+  {
+    "revision": "d9adaa53cff9ae2aa9717dc975602f9f",
+    "url": "/sberform/static/media/circle-cropped (3).d9adaa53.png"
+  },
+  {
+    "revision": "e4dcb7cbb79a961be25665b60942e601",
+    "url": "/sberform/static/media/login_checkmark.e4dcb7cb.svg"
+  },
+  {
+    "revision": "db07693803472cba90a905d818b7e8bb",
+    "url": "/sberform/static/media/login_error.db076938.svg"
+  },
+  {
+    "revision": "a3f99fa99f0d179baf5a4df10c7d0360",
+    "url": "/sberform/static/media/purple_sunset-wallpaper-2880x1800.a3f99fa9.png"
+  }
+]);
